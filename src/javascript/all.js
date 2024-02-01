a1a895cf800c404e6a66006ee9b11e6bb5440774
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-02-01 14:35:09 +0000

    Worked on it a lot.

---
+++ src/javascript/all.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    const currentTitle = document.title;
+    document.title = `${currentTitle} | Macbeth Project / Macbeth into madness...`;
+    console.log('Page has successfully loaded!');
+});
