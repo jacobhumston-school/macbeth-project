459a0d2cf85e2885e1f015da9051a645e2ae2448
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-02-06 14:25:08 +0000

    Finished quotations script.

--- src/javascript/timeline.js
+++ src/javascript/timeline.js
@@ -1,8 +1,18 @@
 function hookQuotes() {
-    body = document.body
-    for (const element in body.) {
-        
+    for (const element of document.body.getElementsByTagName('span')) {
+        if (element.classList.contains('quotation')) {
+            element.dataset.original = element.innerHTML;
+            element.innerHTML = element.innerHTML.replaceAll("'", '"');
+            element.addEventListener('click', function () {
+                if (element.innerHTML !== element.dataset.original.replaceAll("'", '"')) {
+                    element.innerHTML = element.dataset.original;
+                } else {
+                    element.innerHTML = element.dataset.translated;
+                }
+                element.innerHTML = element.innerHTML.replaceAll("'", '"');
+            });
+        }
     }
 }
 
-window.addEventListener("load", hookQuotes)
+window.addEventListener('load', hookQuotes);
