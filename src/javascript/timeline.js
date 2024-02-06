d272bf8c730d982198863277642ae2930e941e28
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-02-06 16:35:50 +0000

    Fixed the quote issue.

--- src/javascript/timeline.js
+++ src/javascript/timeline.js
@@ -1,15 +1,37 @@
+// SOURCE: https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-specific-index-in-javascript
+/**
+ * Replace the character at index with replacement.
+ * @param {number} index
+ * @param {string} replacement
+ * @returns
+ */
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+};
+
+/**
+ * Fix the quotes in a quote string.
+ * @param {string} string
+ */
+function fixQuoteString(string) {
+    return string.replaceAt(0, '"').replaceAt(string.length - 1, '"');
+}
+
+/**
+ * Quote management function.
+ */
 function hookQuotes() {
     for (const element of document.body.getElementsByTagName('span')) {
         if (element.classList.contains('quotation')) {
             element.dataset.original = element.innerHTML;
-            element.innerHTML = element.innerHTML.replaceAll("'", '"');
+            element.innerHTML = fixQuoteString(element.dataset.original);
             element.addEventListener('click', function () {
-                if (element.innerHTML !== element.dataset.original.replaceAll("'", '"')) {
+                if (element.innerHTML !== fixQuoteString(element.dataset.original)) {
                     element.innerHTML = element.dataset.original;
                 } else {
                     element.innerHTML = element.dataset.translated;
                 }
-                element.innerHTML = element.innerHTML.replaceAll("'", '"');
+                element.innerHTML = fixQuoteString(element.innerHTML);
             });
         }
     }
